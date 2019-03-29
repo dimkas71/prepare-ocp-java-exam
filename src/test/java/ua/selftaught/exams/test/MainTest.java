2e78766833ea861259a5839bbe8ab719eb6b2624
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-29 18:21:45 +0200

    Add testing support

---
+++ src/test/java/ua/selftaught/exams/test/MainTest.java
@@ -0,0 +1,13 @@
+package ua.selftaught.exams.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	final void test() {
+		Assertions.assertTrue(true, () -> "Should be true");
+	}
+
+}
