6affae6482104c9c28d9cf35dea15ee5b7e28410
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-29 18:17:45 +0200

    Add an example of using java.util.Properties class

--- src/main/java/ua/selftaught/exams/Main.java
+++ src/main/java/ua/selftaught/exams/Main.java
@@ -1,9 +1,22 @@
 package ua.selftaught.exams;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Properties props = new Properties();
+		
+		try {
+			props.load(Main.class.getClassLoader().getResourceAsStream("app.properties"));
+			System.out.println(props.get("ua.selftaught.name"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
