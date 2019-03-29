9c7c9b051ff739886dbd6c8d1f5fd9d16fed4501
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-29 18:44:49 +0200

    Add new test case

--- src/test/java/ua/selftaught/exams/test/MainTest.java
+++ src/test/java/ua/selftaught/exams/test/MainTest.java
@@ -1,12 +1,16 @@
 package ua.selftaught.exams.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
 	@Test
-	final void test() {
+	@DisplayName("OCA page 34. 4. What is the output of the following program?")
+	void test() {
 		int count = 0;
 		int x = 3;
 		
@@ -20,6 +24,29 @@ class MainTest {
 		}
 		
 		Assertions.assertEquals(6, x, () -> "x should be equals to 6");
+	}
+	
+	@Test
+	@DisplayName("OCA page 35. 5. What is the output of the following code snippet?")
+	void testOCApage35_5() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("a");
+		
+		try {
+			sb.append("b");
+			throw new IllegalArgumentException();
+			
+		} catch (RuntimeException e) {
+			sb.append("c");
+		} finally {
+			sb.append("d");
+		}
+		sb.append("e");
+		
+		
+		assertEquals("abcde", sb.toString(), () -> "Result should be abcde");
+		
+		
 	}
 
 }
