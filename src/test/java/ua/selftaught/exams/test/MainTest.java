b964ace0e17a8b1e24535e5190557c097c39086d
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-29 21:56:43 +0200

    Add OCA tests for pages from 35 to 36

--- src/test/java/ua/selftaught/exams/test/MainTest.java
+++ src/test/java/ua/selftaught/exams/test/MainTest.java
@@ -1,6 +1,10 @@
 package ua.selftaught.exams.test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +14,7 @@ class MainTest {
 
 	@Test
 	@DisplayName("OCA page 34. 4. What is the output of the following program?")
-	void test() {
+	void testOCApage34_4() {
 		int count = 0;
 		int x = 3;
 		
@@ -48,5 +52,62 @@ class MainTest {
 		
 		
 	}
+	
+	@Test
+	@DisplayName("OCA page 35. 6. What is the result of following program?")
+	void testOCApage35_6() {
+		
+		class MainFunctions {
+			private void addToInt(int x, int amountToAdd) {
+				x = x + amountToAdd;
+			}
+		}
+		
+		int a = 15;
+		int b = 10;
+		
+		new MainFunctions().addToInt(a, b);
+		
+		assertEquals(15, a);
+		
+	}
+	
+	
+	@Test
+	@DisplayName("OCA page 35. 7. What is the result of the following code?")
+	void testOCApage35_7() {
+		int[] array = {6, 9, 8};
+		
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(array[0]);
+		list.add(array[2]);
+		list.set(1, array[1]);
+		
+		list.remove(0);
+		
+		assertEquals("[9]", list.toString(), () -> "should be [9]");
+		
+		
+	}
+	
+	@Test
+	void test() {
+		class Deer {
+			public Deer() {System.out.print("Deer");}
+			public Deer(int age) { System.out.print("DeepAge");}
+			private boolean hasHorns() {return false;}
+		}
+		
+		class Reinder extends Deer {
+			public Reinder(int age) {System.out.print("Reindeer");}
+			public boolean hasHorns() { return true;}
+		}
+		
+		Deer deer = new Reinder(5);
+		System.out.println("," + deer.hasHorns());
+		
+	}
+	
 
 }
