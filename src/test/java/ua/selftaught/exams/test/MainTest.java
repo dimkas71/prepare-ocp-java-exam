b25e5d699ac3b90eb71fc15b2f5df791cdbd5d8e
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-29 18:29:42 +0200

    Add a test from OCA books page 34 4. What is the output of the following program?

--- src/test/java/ua/selftaught/exams/test/MainTest.java
+++ src/test/java/ua/selftaught/exams/test/MainTest.java
@@ -7,7 +7,19 @@ class MainTest {
 
 	@Test
 	final void test() {
-		Assertions.assertTrue(true, () -> "Should be true");
+		int count = 0;
+		int x = 3;
+		
+		while (count++ < 3) {
+			int y = (1 + 2 * count) % 3;
+			switch (y) {
+				default:
+				case 0: x -= 1; break;
+				case 1: x += 5;
+			}
+		}
+		
+		Assertions.assertEquals(6, x, () -> "x should be equals to 6");
 	}
 
 }
