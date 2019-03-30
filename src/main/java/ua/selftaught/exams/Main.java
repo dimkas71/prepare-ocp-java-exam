94a2b5821086b92cd2c031ea828183863e0eb90d
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-30 17:12:33 +0200

    base point for hibernate learning

--- src/main/java/ua/selftaught/exams/Main.java
+++ src/main/java/ua/selftaught/exams/Main.java
@@ -1,25 +1,10 @@
 package ua.selftaught.exams;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Properties props = new Properties();
 		
-		try {
-			props.load(Main.class.getClassLoader().getResourceAsStream("app.properties"));
-			System.out.println(props.get("ua.selftaught.name"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		
-		
+		System.out.println("Hello world");
 		
 	}
 
