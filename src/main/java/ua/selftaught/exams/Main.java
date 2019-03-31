cd8f0a04ac87cf5b48bda3c0500b5f4786b553e5
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-03-31 16:12:23 +0300

    Remove test cod

--- src/main/java/ua/selftaught/exams/Main.java
+++ src/main/java/ua/selftaught/exams/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		String A$B = "abc";
+		String s2 = new String("abc");
+		
+		if (A$B == s2) 
+			System.out.print(1);
+		else 
+			System.out.println(2);
+		
+		if (A$B.equals(s2))
+			System.out.println(3);
+		else
+			System.out.println(4);
 		
 	}
 
